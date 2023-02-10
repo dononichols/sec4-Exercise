354c393345c761b79bb4d25b0d1305c025a1e95a
Meraki_DNA <dononichols@gmail.com>
2023-02-10 15:11:20 -0500

    jsTrainingmosh

---
+++ index.js
@@ -0,0 +1,16 @@
+
+// Exercise ___ vid.10
+// Max of Two Numbers
+
+// Write a function that takes two numbers and returns the maximum of the two.
+
+let number = max(6, 5);
+console.log(number);
+
+function max(a, b) {
+    // if (a > b) return a;
+    // return b;
+
+    // or write it like this
+    return (a>b) ? a : b;
+}
